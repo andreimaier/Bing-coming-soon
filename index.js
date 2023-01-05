9f73560bbb8abd7c6127d1e559055d970d642ffe
andreimaier <113696878+andreimaier@users.noreply.github.com>
2023-01-05 11:11:04 +0800

    final

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-const input = document.querySelector("input")
+const form = document.querySelector("#form")
+const email = document.querySelector('input')
+const error = document.querySelector('.error')
 
-console.log(input)
 
 
-input.addEventListener('input', () => {
-    if(/^[0-9]+$/.test(input.value)) {
-        console.log('correct')
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(regex.test(email.value)) {
+        console.log('lala')
+    }   else {
+        console.log('sorry')
+    }
+})
+
+
+
+
+
+
+
+
+        /* console.log('correct')
         input.style.outline = "2px solid Highlight"
         input.style.borderColor = "#c2d3ff"
+        error.style.opacity = "0";
+        
     } else {
         console.log('else')
         input.style.borderColor = "red"
         input.style.outline = "1px solid transparent"
+        error.style.opacity = "1";
         
-    }
-})
+    } */
+
+
+
+
+/* })
 input.addEventListener('focusin', () => {
     input.style.outline = "2px solid Highlight"
     input.style.borderColor = "#c2d3ff"
 }) 
 input.addEventListener('focusout', () => {
     input.style.outline = "none"
-}) 
+})  */
